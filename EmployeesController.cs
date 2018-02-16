4d792d8c299186d067f8698c953e1be60cce31d6
kulasekaralwar <35416340+kulasekaralwar@users.noreply.github.com>
2018-02-16 10:25:09 +0530

    Add files via upload

---
+++ EmployeesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUDEg.Models;
+
+namespace CRUDEg.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private MyOrgEntities db = new MyOrgEntities();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return Json(db.Employees.ToList(),JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            Employee employee = db.Employees.Find(id);
+            return Json(employee,JsonRequestBehavior.AllowGet);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return Json(employee,JsonRequestBehavior.AllowGet);
+        }
+
+
+         // POST: Employees/Create
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return Json(employee, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(ModelState.Keys
+                    .SelectMany(k => ModelState[k].Errors)
+                    .Select(m => m.ErrorMessage).ToArray(),
+                    JsonRequestBehavior.AllowGet);
+
+            }
+        }
+
+           // POST: Employees/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+          
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return Json(employee, JsonRequestBehavior.AllowGet);
+           
+        }
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ myapp.js
@@ -0,0 +1,115 @@
+﻿var app = angular.module('MyApp', ['angularUtils.directives.dirPagination']);
+
+app.factory('crudServiceEmployees', function ($http) {
+    crudEmpObj = {};
+
+    crudEmpObj.getAll = function () {
+        var Emps;
+
+        Emps = $http({ method: 'Get', url: '/Employees/Index' }).
+        then(function (response) {
+            return response.data;
+        });
+
+        return Emps;
+    };
+
+    crudEmpObj.getByEid = function (eid) {
+        var Emp;
+
+        Emp = $http({ method: 'Get', url: '/Employees/Details/', params: { id: eid } }).
+        then(function (response) {
+            return response.data;
+        });
+
+        return Emp;
+    };
+
+    crudEmpObj.deleteByEid = function (eid) {
+        var Emp;
+
+        Emp = $http({ method: 'Post', url: '/Employees/Delete/', params: { id: eid } }).
+        then(function (response) {
+            return response.data;
+        });
+
+        return Emp;
+    };
+
+    crudEmpObj.createEmployee = function (emp) {
+        var Emp;
+
+        Emp = $http({ method: 'Post', url: '/Employees/Create/', data: emp }).
+        then(function (response) {
+            return response.data;
+        });
+
+        return Emp;
+    };
+
+    crudEmpObj.updateEmployee = function (emp) {
+        var Emp;
+
+        Emp = $http({ method: 'Post', url: '/Employees/Edit/', data: emp }).
+        then(function (response) {
+            return response.data;
+        });
+
+        return Emp;
+    };
+
+    return crudEmpObj;
+});
+
+app.controller('employeesController', function ($scope, crudServiceEmployees) {
+
+    crudServiceEmployees.getAll().then(function (result) {
+        $scope.Emps = result;
+    });
+
+    $scope.GetByEid = function(eid) {
+        crudServiceEmployees.getByEid(eid).then(function (result) {
+            result.EDOB = new Date(parseInt(result.EDOB.substr(6)));
+            $scope.Emp = result;
+        });
+    };
+
+    $scope.DeleteByEid = function (eid) {
+        crudServiceEmployees.deleteByEid(eid).then(function (result) {
+            $scope.Msg = result.EName +"Has been deleted successfully";
+
+            crudServiceEmployees.getAll().then(function (result) {
+                $scope.Emps = result;
+            });
+        });        
+    };
+
+    $scope.CreateEmployee = function (Emp, isValid) {
+        if (isValid) {
+            crudServiceEmployees.createEmployee(Emp).then(function (result) {
+                if (result.Eid != null) {
+                    $scope.Msg = result.EName + " Has been created successfully";
+
+                    crudServiceEmployees.getAll().then(function (result) {
+                        $scope.Emps = result;
+
+                    });
+                }
+                else {
+                    alert(result);
+                    $scope.errorMsgs = result;
+                }
+            });
+        }
+    };
+
+    $scope.UpdateEmployee = function (Emp) {
+        crudServiceEmployees.updateEmployee(Emp).then(function (result) {
+            $scope.Msg = result.EName + " Has been updated successfully";
+
+            crudServiceEmployees.getAll().then(function (result) {
+                $scope.Emps = result;
+            });
+        });
+    };
+});
