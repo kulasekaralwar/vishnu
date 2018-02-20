60753f3695a92d41adbfa09a8cb6cf2b2ca005c8
kulasekaralwar <35416340+kulasekaralwar@users.noreply.github.com>
2018-02-20 09:36:51 +0530

    Add files via upload

---
+++ WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace LMS.Server
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            ICorsPolicyProvider policy = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(policy);
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new Hosta)
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
